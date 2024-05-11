0b1cb41dbda012f7ffc28f52830b076711bc6ff3
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-05-11 17:34:18 +0530

    side bar data fixed for user-driven apporoach

--- src/components/RecentVisitor.js
+++ src/components/RecentVisitor.js
@@ -20,7 +20,7 @@ const RecentVisitor = () => {
 
     return (
         <div className='h-20 w-full border-b-2 flex gap-2 items-center pl-10 py-2 justify-between'>
-            <h3 className='text-3xl font-semibold text-blue-950'>Recent visitor {dataItem && dataItem.length}</h3>
+            <h3 className='text-3xl font-semibold text-blue-950'>Recent visitor - ({dataItem && dataItem.length})</h3>
             <div className='h-20 w-1/2  flex gap-2 items-center pl-10'>
                 <img className='h-6 cursor-pointer' src={SearchB} alt='search' onClick={() => handleClick()}/>
                 <input className='h-12 w-full text-lg outline-none focus:border-b-2' type='text' name='search' placeholder='Search' onChange={(e) => handleChange(e)}/>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Repo from '../assets/Repo.png'
+import UserLogo from '../assets/UserLogoB.png'
 const Sidebar = () => {
   const [isOpenSidebar, setSideBar] = useState(true)
   return (
@@ -9,23 +10,23 @@ const Sidebar = () => {
             <img className='h-7 ' src={Repo} alt='-'/>
             <p className='font-semibold uppercase text-gray-700'>My Repositories</p>
           </div>
-          <span className='h-6 w-6 bg-blue-600 flex items-center justify-center text-white rounded-md'>1</span>
+          <span className='h-6 w-6 bg-blue-600 flex items-center justify-center text-white rounded-md'>10</span>
         </div>
-        <div className='flex items-center justify-between'>
+        <div className='flex items-center justify-between hover:cursor-pointer'>
           <div className='h-16 w-fit flex gap-4 items-center'>
-            <img className='h-7 ' src={Repo} alt='-'/>
-            <p className='font-semibold uppercase text-gray-700'>My Repositories</p>
+            <img className='h-7 ' src={UserLogo} alt='-'/>
+            <p className='font-semibold uppercase text-gray-700'>Go to Github Profile</p>
           </div>
-          <span className='h-6 w-6 bg-green-600 flex items-center justify-center text-white rounded-md'>1</span>
+          <span className='h-6 w-6 bg-green-600 flex items-center justify-center text-white rounded-md'></span>
         </div>
         <span className='border-2'></span>
         <p className='font-semibold uppercase '>Menu</p>
         <div className='flex items-center justify-between'>
           <div className='h-16 w-fit flex gap-4 items-center'>
             <img className='h-7 ' src={Repo} alt='-'/>
-            <p className='font-semibold uppercase text-gray-700'>My Repositories</p>
+            <p className='font-semibold uppercase text-gray-700'>About US</p>
           </div>
-          <span className='h-6 w-6 bg-green-600 flex items-center justify-center text-white rounded-md'>1</span>
+          <span className='h-6 w-6 bg-green-600 flex items-center justify-center text-white rounded-md'></span>
         </div>
         <div className='flex items-center justify-between'>
           <div className='h-16 w-fit flex gap-4 items-center'>
